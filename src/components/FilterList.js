2734f306ff8448e57a311d39df8d383acde99b97
wcolts2000 <wcolts2000@gmail.com>
2020-02-02 14:35:54 -0800

    adding prop-types to project

--- src/components/FilterList.js
+++ src/components/FilterList.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function FilterList() {
+export default function FilterList({ filterFoods }) {
   const handleSubmit = function(event) {
     event.preventDefault();
     alert('hit');
@@ -8,10 +9,19 @@ export default function FilterList() {
   return (
     <form id="filterList" onSubmit={handleSubmit}>
       <label htmlFor="searchInput">Search Foods</label>
-      <input type="text" placeholder="Search..." name="searchInput" />
+      <input
+        type="text"
+        placeholder="Search..."
+        name="searchInput"
+        onChange={filterFoods}
+      />
       <button type="submit" name="submit">
         Search Now
       </button>
     </form>
   );
 }
+
+FilterList.propTypes = {
+  filterFoods: PropTypes.func.isRequired
+};
