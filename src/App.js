b25790728df8ce5d7cd8789bf7559114e81e8457
wcolts2000 <wcolts2000@gmail.com>
2020-02-02 03:09:17 -0800

    loading dataset into a table component for now.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [nutrients, setNutrients] = useState([]);
   useEffect(() => {
-    csv('abbrev-food-nutritional-values-QueryResult.csv').then(data => {
+    csv('food-nutritional-values.csv').then(data => {
       console.log('***DATA***', data);
       setLoading(false);
       setNutrients(data);
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-container">
         <div>
           {loading ? (
             <>
@@ -24,7 +24,44 @@ function App() {
               <p>Loading...</p>
             </>
           ) : (
-            <p>{JSON.stringify(nutrients)}</p>
+            <table>
+              <thead>
+                <tr className="tHeaders">
+                  <th>Short Description</th>
+                  <th>Calories</th>
+                  <th>Protein</th>
+                  <th>Carbohydrates</th>
+                  <th>Fiber</th>
+                  <th>Sugar</th>
+                  <th>Saturated Fats</th>
+                  <th>Monosaturated Fats</th>
+                  <th>Polysaturated Fats</th>
+                  <th>Weight 1</th>
+                  <th>Weight 1 Description</th>
+                  <th>Weight 2</th>
+                  <th>Weight 2 Description</th>
+                </tr>
+              </thead>
+              <tbody>
+                {nutrients.map((food, i) => (
+                  <tr key={i}>
+                    <td>{food.shrt_desc}</td>
+                    <td>{food.energ_kcal}</td>
+                    <td>{food.protein_g}</td>
+                    <td>{food.carbohydrt_g}</td>
+                    <td>{food.fiber_td_g}</td>
+                    <td>{food.sugar_tot_g}</td>
+                    <td>{food.fa_sat_g}</td>
+                    <td>{food.fa_mono_g}</td>
+                    <td>{food.fa_poly_g}</td>
+                    <td>{food.gmwt_1}</td>
+                    <td>{food.gmwt_desc1}</td>
+                    <td>{food.gmwt_2}</td>
+                    <td>{food.gmwt_desc2}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           )}
         </div>
       </header>
